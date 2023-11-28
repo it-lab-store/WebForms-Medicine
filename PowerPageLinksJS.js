c93405ee7cbd536e57daffea8e7f99ee33ce351e
Евгений <immortal.27@yandex.ru>
2023-11-28 09:04:43 +0300

    webforms files added

---
+++ PowerPageLinksJS.js
@@ -0,0 +1,19 @@
+function OpenLinkPageOftalm() // link
+{
+window.open("http://localhost/OsmotrOftalmologa/favorites");
+}
+
+function OpenLinkPageJournal()
+{
+window.open("http://localhost/OsmotrOftalmologa/report/Rep/ZhurnalOsmotrov");
+}
+
+function OpenLinkPageTableOfCodes()
+{
+window.open("http://localhost/OsmotrOftalmologa/report/Rep/Коды%20МКБ10");
+}
+
+function OpenLinkStartOsmotr()
+{
+window.open("http://127.0.0.1:108");
+}
